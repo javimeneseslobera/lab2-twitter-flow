240735e2895eb62f798256abccff2a7efc2fcd93
Javi Meneses <jmeneses@quelinka.com>
2018-04-17 23:26:15 +0200

    P2 done

--- src/main/java/es/unizar/tmdad/lab2/configuration/TwitterFlow.java
+++ src/main/java/es/unizar/tmdad/lab2/configuration/TwitterFlow.java
@@ -1,5 +1,9 @@
 package es.unizar.tmdad.lab2.configuration;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +12,15 @@ import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowBuilder;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.social.twitter.api.StreamListener;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.web.util.HtmlUtils;
+
+import es.unizar.tmdad.lab2.domain.MyTweet;
+import es.unizar.tmdad.lab2.domain.TargetedTweet;
+import es.unizar.tmdad.lab2.service.TwitterLookupService;
 
 @Configuration
 @EnableIntegration
@@ -22,29 +33,57 @@ public class TwitterFlow {
 		return new DirectChannel();
 	}
 
+	@Autowired
+	private TwitterLookupService lookupService;
+
 	// Tercer paso
-	// Los mensajes se leen de "requestChannel" y se envian al método "sendTweet" del
+	// Los mensajes se leen de "requestChannel" y se envian al método "sendTweet"
+	// del
 	// componente "streamSendingService"
 	@Bean
 	public IntegrationFlow sendTweet() {
-        //
-        // CAMBIOS A REALIZAR:
-        //
-        // Usando Spring Integration DSL
-        //
-        // Filter --> asegurarnos que el mensaje es un Tweet
-        // Transform --> convertir un Tweet en un TargetedTweet con tantos tópicos como coincida
-        // Split --> dividir un TargetedTweet con muchos tópicos en tantos TargetedTweet como tópicos haya
-        // Transform --> señalar el contenido de un TargetedTweet
-        //
-		return IntegrationFlows.from(requestChannel()).
-				handle("streamSendingService", "sendTweet").get();
+		//
+		// CAMBIOS A REALIZAR:
+		//
+		// Usando Spring Integration DSL
+		//
+		// Filter --> asegurarnos que el mensaje es un Tweet
+		// Transform --> convertir un Tweet en un TargetedTweet con tantos tópicos como
+		// coincida
+		// Split --> dividir un TargetedTweet con muchos tópicos en tantos
+		// TargetedTweet como tópicos haya
+		// Transform --> señalar el contenido de un TargetedTweet
+		//
+		IntegrationFlowBuilder integrationFlowBuilder = IntegrationFlows.from(requestChannel());
+		integrationFlowBuilder = integrationFlowBuilder.filter(t -> t instanceof Tweet);
+		integrationFlowBuilder = integrationFlowBuilder.transform(Tweet.class, t -> TransformToTargetedTweet(t));
+		integrationFlowBuilder = integrationFlowBuilder.split(TargetedTweet.class, t -> SplitTargetedTweet(t));
+		integrationFlowBuilder = integrationFlowBuilder.transform(TargetedTweet.class, t -> PointOutTweetContent(t));
+		return integrationFlowBuilder.handle("streamSendingService", "sendTweet").get();
+	}
+
+	private TargetedTweet TransformToTargetedTweet(Tweet t) {
+		MyTweet tweet = new MyTweet(t);
+		List<String> topics = lookupService.getQueries().stream().filter(key -> t.getText().contains(key))
+				.collect(Collectors.toList());
+		return new TargetedTweet(tweet, topics);
+	}
+
+	private List<TargetedTweet> SplitTargetedTweet(TargetedTweet t) {
+		return t.getTargets().stream().map(key -> new TargetedTweet(t.getTweet(), key)).collect(Collectors.toList());
+	}
+
+	private TargetedTweet PointOutTweetContent(TargetedTweet t) {
+		t.getTweet().setUnmodifiedText(t.getTweet().getUnmodifiedText().replaceAll(t.getFirstTarget(),
+				"<b>" + HtmlUtils.htmlEscape(t.getFirstTarget()) + "</b>"));
+		return t;
 	}
 
 }
 
 // Segundo paso
-// Los mensajes recibidos por este @MessagingGateway se dejan en el canal "requestChannel"
+// Los mensajes recibidos por este @MessagingGateway se dejan en el canal
+// "requestChannel"
 @MessagingGateway(name = "integrationStreamListener", defaultRequestChannel = "requestChannel")
 interface MyStreamListener extends StreamListener {
 
--- src/main/java/es/unizar/tmdad/lab2/service/StreamSendingService.java
+++ src/main/java/es/unizar/tmdad/lab2/service/StreamSendingService.java
@@ -1,5 +1,13 @@
 package es.unizar.tmdad.lab2.service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -11,29 +19,20 @@ import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MimeTypeUtils;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.function.Consumer;
-
-import javax.annotation.PostConstruct;
-
 import es.unizar.tmdad.lab2.domain.TargetedTweet;
 
 @Service
 public class StreamSendingService {
-	
 
 	@Autowired
 	private SimpMessageSendingOperations ops;
-	
+
 	@Autowired
 	private TwitterTemplate twitterTemplate;
 
 	@Autowired
 	private TwitterLookupService lookupService;
-	
+
 	private Stream stream;
 
 	@Autowired
@@ -47,11 +46,13 @@ public class StreamSendingService {
 		// Primer paso
 		// Registro un gateway para recibir los mensajes
 		// Ver @MessagingGateway en MyStreamListener en TwitterFlow.java
-		stream = twitterTemplate.streamingOperations().filter(fsp, Collections.singletonList(integrationStreamListener));
+		stream = twitterTemplate.streamingOperations().filter(fsp,
+				Collections.singletonList(integrationStreamListener));
 	}
 
 	// Cuarto paso
-	// Recibe un tweet y hay que enviarlo a tantos canales como preguntas hay registradas en lookupService
+	// Recibe un tweet y hay que enviarlo a tantos canales como preguntas hay
+	// registradas en lookupService
 	//
 	public void sendTweet(Tweet tweet) {
 		Map<String, Object> map = new HashMap<>();
@@ -71,9 +72,10 @@ public class StreamSendingService {
 		//
 		// Crea un mensaje que envie un tweet a un único tópico destinatario
 		//
-
+		Map<String, Object> map = new HashMap<>();
+		map.put(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON);
+		ops.convertAndSend("/queue/search/" + tweet.getFirstTarget(), tweet.getTweet(), map);
 	}
-
 
 	public Stream getStream() {
 		return stream;
